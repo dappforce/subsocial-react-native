0fb35ee12a09c1d430ceaa97ee0c5a154311f77d
Kiruse <contact@kiruse.dev>
2021-10-01 17:05:57 +0200

    made welcome story TSX :)

---
+++ storybook/stories/Welcome/index.tsx
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+export interface WelcomeProps {
+  showApp?: () => void;
+}
+
+export default class Welcome extends Component<WelcomeProps> {
+  render(): React.ReactNode {
+    const { showApp } = this.props;
+    
+    return (
+      <View style={styles.container}>
+        <Text style={styles.header}>Welcome to React Native Storybook</Text>
+        <Text style={styles.content}>
+          This is a UI Component development environment for your React Native app. Here you can
+          display and interact with your UI components as stories. A story is a single state of one
+          or more UI components. You can have as many stories as you want. In other words a story is
+          like a visual test case.
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    justifyContent: 'center',
+  },
+  header: {
+    fontSize: 18,
+    marginBottom: 18,
+  },
+  content: {
+    fontSize: 12,
+    marginBottom: 10,
+    lineHeight: 18,
+  },
+});
