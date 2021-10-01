5fa17e9b73a75b8e59711d8258958fa4570138c9
Kiruse <contact@kiruse.dev>
2021-10-01 15:56:25 +0200

    fix incorrect safe area offsets

--- App.tsx
+++ App.tsx
@@ -1,3 +1,21 @@
-// currently, storybook only
-// TODO: render full app
-export {default} from './storybook'
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import StorybookUI from './storybook';
+
+export default function(props: Object) {
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.container}>
+        <StorybookUI {...props} />
+      </SafeAreaView>
+    </SafeAreaProvider>
+  )
+};
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    height: '100%',
+  },
+});
