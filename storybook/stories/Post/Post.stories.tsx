2349da9045a438ef60b32371585ba2cbc1571857
Kiruse <contact@kiruse.dev>
2021-10-06 16:06:53 +0200

    clean imports

--- storybook/stories/Post/Post.stories.tsx
+++ storybook/stories/Post/Post.stories.tsx
@@ -1,12 +1,10 @@
 import React from 'react'
-import { Text } from 'react-native'
 import { action } from '@storybook/addon-actions'
 import { text } from '@storybook/addon-knobs'
 import { storiesOf } from '@storybook/react-native'
 import Post from './index'
-import { SubstrateProvider, SubsocialProvider } from '../../../src/components/SubsocialContext'
+import { SubsocialProvider } from '../../../src/components/SubsocialContext'
 import CenterView from '../CenterView'
-import config from 'config.json'
 
 storiesOf('Post', module)
   .addDecorator((getStory) => (
