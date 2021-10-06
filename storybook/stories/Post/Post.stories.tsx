841eb51c0c015806145de1c238152b3702f74c32
Kiruse <contact@kiruse.dev>
2021-10-06 16:06:28 +0200

    first post interface wooh!

--- storybook/stories/Post/Post.stories.tsx
+++ storybook/stories/Post/Post.stories.tsx
@@ -4,11 +4,18 @@ import { action } from '@storybook/addon-actions'
 import { text } from '@storybook/addon-knobs'
 import { storiesOf } from '@storybook/react-native'
 import Post from './index'
+import { SubstrateProvider, SubsocialProvider } from '../../../src/components/SubsocialContext'
 import CenterView from '../CenterView'
+import config from 'config.json'
 
 storiesOf('Post', module)
-  .addDecorator((getStory) => <CenterView>{getStory()}</CenterView>)
-  .add('some story', () => {
-    return <Text>:D</Text>
+  .addDecorator((getStory) => (
+    <SubsocialProvider>
+      <CenterView>
+        {getStory()}
+      </CenterView>
+    </SubsocialProvider>
+  ))
+  .add('sample post', () => {
+    return <Post id={20423} />
   })
-  ;
--- storybook/stories/Post/index.tsx
+++ storybook/stories/Post/index.tsx
@@ -1,5 +1,79 @@
-import React, { ReactNode } from 'react'
+import React, { ReactNode, useCallback, useState } from 'react'
+import { StyleSheet, View, Text } from 'react-native'
+import { useSubsocial } from '../../../src/components/SubsocialContext'
+import { PostData } from '@subsocial/types/dto'
+import BN from 'bn.js'
 
-export default function(): ReactNode {
-  return null;
+export type PostProps = {
+  id: number
 }
+
+type PostStateJob = 'PENDING' | 'LOADING' | 'READY' | 'ERROR'
+export type PostState = {
+  job: PostStateJob
+  data?: PostData
+  error?: any
+}
+
+export class PostNotFoundError extends Error {
+  constructor(postId: number) {
+    super(`Post ${postId} not found`);
+  }
+}
+
+export default function Post({id}: PostProps) {
+  const {api} = useSubsocial() ?? {};
+  const [state, setState] = useState<PostState>({job: 'PENDING'});
+  const loadingStyle = (state.job === 'PENDING' || state.job === 'LOADING') && styles.loading;
+  const errorStyle   =  state.job === 'ERROR' && styles.error;
+  
+  useCallback(async () => {
+    if (!api) return;
+    
+    if (state.job === 'PENDING') {
+      setState({...state, job: 'LOADING'});
+      try {
+        const data = await api.findPost({id: new BN(id)});
+        if (data)
+          setState({...state, job: 'READY', data});
+        else
+          setState({...state, job: 'ERROR', error: new PostNotFoundError(id)});
+      }
+      catch (err: any) {
+        setState({...state, job: 'ERROR', error: err});
+      }
+    }
+  }, [api, id, state, setState])();
+  
+  return (
+    <View style={styles.container}>
+      <Text style={[loadingStyle, styles.title]}>{state.data?.content?.title ?? 'loading ...'}</Text>
+      <Text style={[loadingStyle, errorStyle, styles.content]}>
+        {state.job === 'ERROR'
+          ? 'An error occured while loading the post\'s contents.'
+          : state.data?.content?.body || 'loading ...'
+        }
+      </Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    padding: 10,
+  },
+  title: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  content: {
+    
+  },
+  loading: {
+    fontStyle: 'italic',
+  },
+  error: {
+    fontStyle: 'italic',
+  },
+});
