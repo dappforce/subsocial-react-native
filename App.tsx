52c9c44fe71ad1afc8ea61277041e6b1fc3839fd
Kiruse <contact@kiruse.dev>
2021-10-07 13:41:45 +0200

    apply app background color

--- App.tsx
+++ App.tsx
@@ -1,15 +1,20 @@
-import React from 'react'
-import { StyleSheet } from 'react-native'
+import React, { useMemo } from 'react'
+import { ColorSchemeName, StyleSheet, useColorScheme } from 'react-native'
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context'
+import { StatusBar } from 'expo-status-bar'
 import { SubstrateProvider } from './src/components/SubstrateContext'
 import StorybookUI from './storybook'
 import config from 'config.json'
 
 
-export default function(props: Object) {
+export default function(props: {}) {
+  const scheme = useColorScheme();
+  const styles = useMemo(() => createStyles(scheme), [scheme]);
+  
   return (
     <SubstrateProvider endpoint={config.connections.ws.substrate}>
       <SafeAreaProvider>
+        <StatusBar backgroundColor={backgroundColor(scheme)} />
         <SafeAreaView style={styles.container}>
           <StorybookUI {...props} />
         </SafeAreaView>
@@ -18,8 +23,10 @@ export default function(props: Object) {
   )
 };
 
-const styles = StyleSheet.create({
+const backgroundColor = (scheme: ColorSchemeName) => scheme === 'light' ? 'hsl(271, 84%, 92%)' : 'hsl(250, 10%, 4%)';
+const createStyles = (scheme: ColorSchemeName) => StyleSheet.create({
   container: {
+    backgroundColor: backgroundColor(scheme),
     width: '100%',
     height: '100%',
   },
