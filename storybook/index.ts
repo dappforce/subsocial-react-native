a7baa7f35ef38a2eea6af2203498f704225feece
Kiruse <contact@kiruse.dev>
2021-10-07 15:40:10 +0200

    clean up storybook index

--- storybook/index.ts
+++ storybook/index.ts
@@ -1,4 +1,3 @@
-// if you use expo remove this line
 import { getStorybookUI, configure, addDecorator } from '@storybook/react-native'
 import { withKnobs } from '@storybook/addon-knobs'
 
@@ -12,10 +11,6 @@ configure(() => {
   require('./stories');
 }, module);
 
-// Refer to https://github.com/storybookjs/storybook/tree/master/app/react-native#start-command-parameters
-// to find allowed options for getStorybookUI
-const StorybookUIRoot = getStorybookUI({
-  asyncStorage: null,
+export default getStorybookUI({
+    asyncStorage: null,
 });
-
-export default StorybookUIRoot;
