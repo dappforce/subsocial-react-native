12074b09329813fc7bf2d08368503467e520ddda
Kiruse <contact@kiruse.dev>
2021-10-07 15:38:48 +0200

    add bg color ctx so it can be used anywhere probably replace with themeing somewhere down the line... maybe @emotion/theming?

--- App.tsx
+++ App.tsx
@@ -3,30 +3,34 @@ import { ColorSchemeName, StyleSheet, useColorScheme } from 'react-native'
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context'
 import { StatusBar } from 'expo-status-bar'
 import { SubstrateProvider } from './src/components/SubstrateContext'
+import { BackgroundColorProvider } from './src/components/BackgroundColorContext'
 import StorybookUI from './storybook'
 import config from 'config.json'
 
 
 export default function(props: {}) {
   const scheme = useColorScheme();
-  const styles = useMemo(() => createStyles(scheme), [scheme]);
+  const bgc    = backgroundColor(scheme);
+  const styles = useMemo(() => createStyles(bgc), [scheme]);
   
   return (
-    <SubstrateProvider endpoint={config.connections.ws.substrate}>
-      <SafeAreaProvider>
-        <StatusBar backgroundColor={backgroundColor(scheme)} />
-        <SafeAreaView style={styles.container}>
-          <StorybookUI {...props} />
-        </SafeAreaView>
-      </SafeAreaProvider>
-    </SubstrateProvider>
+    <BackgroundColorProvider color={bgc}>
+      <SubstrateProvider endpoint={config.connections.ws.substrate}>
+        <SafeAreaProvider>
+          <StatusBar backgroundColor={bgc} />
+          <SafeAreaView style={styles.container}>
+            <StorybookUI {...props} />
+          </SafeAreaView>
+        </SafeAreaProvider>
+      </SubstrateProvider>
+    </BackgroundColorProvider>
   )
 };
 
-const backgroundColor = (scheme: ColorSchemeName) => scheme === 'light' ? 'hsl(271, 84%, 92%)' : 'hsl(250, 10%, 4%)';
-const createStyles = (scheme: ColorSchemeName) => StyleSheet.create({
+const backgroundColor = (scheme: ColorSchemeName) => scheme === 'light' ? '#f6f3ff' : '#0d0319';
+const createStyles = (bgc: string) => StyleSheet.create({
   container: {
-    backgroundColor: backgroundColor(scheme),
+    backgroundColor: bgc,
     width: '100%',
     height: '100%',
   },
