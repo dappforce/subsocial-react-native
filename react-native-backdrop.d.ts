b000ea545bad01498fd405a4c5382c49a1896f34
Kiruse <contact@kiruse.dev>
2021-10-14 18:21:58 +0200

    I give up. Type decl in root now, because TypeScript really fucking hates me I guess

---
+++ react-native-backdrop.d.ts
@@ -0,0 +1,69 @@
+
+import React from 'react'
+import * as RN from 'react-native'
+
+declare module 'react-native-backdrop' {
+  export type AnimationConfig = {
+    /** default: 14 */
+    speed: number
+    /** default: 4 */
+    bounciness: number
+  }
+
+  export type SwipeConfig = {
+    /** Velocity that has to be breached in order for swipe to be triggered (`vx` and `vy` properties of `gestureState`). Default: 0.3 */
+    velocityThreshold: number
+    
+    /** Absolute offset that shouldn't be breached for swipe to be triggered (`dy` for horizontal swipe, `dx` for vertical swipe). Default: 80 */
+    directionalOffsetThreshold: number
+  }
+
+  export type BackdropProps = {
+    /** Content of backdrop (required) */
+    children: React.ReactNode
+    
+    /** Content of backdrop (required) */
+    visible: boolean
+    
+    /** Function to open backdrop (required, function) */
+    handleOpen: () => {}
+    
+    /** Function to close backdrop (required, function) */
+    handleClose: () => {}
+    
+    /** Callback that is called before close animation */
+    beforeClose?: () => {}
+    
+    /** Callback that is called after close animation */
+    afterClose?: () => {}
+    
+    /** Callback that is called before open animation */
+    beforeOpen?: () => {}
+    
+    /** Callback that is called after open animation */
+    afterOpen?: () => {}
+    
+    /** Configures Open and Close Animation speed and bounciness */
+    animationConfig: AnimationConfig
+    
+    /** Configures Swipe Gesture to close backdrop */
+    swipeConfig: SwipeConfig
+    
+    /** Style object for backdrop styling */
+    backdropStyle: RN.StyleProp<RN.ViewStyle>
+    
+    /** Style object for container styling */
+    containerStyle: RN.StyleProp<RN.ViewStyle>
+    
+    /** Display custom header in backdrop */
+    header: () => React.ReactElement
+    
+    /** Height of closed backdrop that will be visible and touchable. Default: 0 */
+    closedHeight: number
+    
+    /** Close backdrop on back button press on android. Default: false */
+    closeOnBackButton: boolean
+  }
+
+  export type Backdrop = React.FC<BackdropProps> | React.Component<BackdropProps>
+}
