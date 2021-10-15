7ccaf90c585d87b828e5e230d3a0aa7bbad99054
Kiruse <contact@kiruse.dev>
2021-10-15 13:12:56 +0200

    unfollowed/followed state

--- storybook/stories/Actions/FollowButton.tsx
+++ storybook/stories/Actions/FollowButton.tsx
@@ -4,21 +4,26 @@
 // TODO: Following requires SUB -> requires transactions -> requires wallet
 // TODO: When following, switch mode to "outline"
 import React, { useCallback } from 'react'
+import { StyleProp, TextStyle } from 'react-native'
 import { Icon } from 'react-native-elements'
 import { Button, ButtonProps } from '~comps/Typography'
 
 export type FollowButtonProps<T> = {
   /** Context-sensitive ID to pass back upon press. The context should know what the ID belongs to (Account, Space). */
   id: T
-  onPress: (id: T) => void
+  onPress: (props: {id: T, isFollowing: boolean}) => void
+  isFollowing: boolean
+  loading?: boolean
   hideIcon?: boolean
+  labelStyle?: StyleProp<TextStyle>
 }
-export default function FollowButton<T>({id, onPress, hideIcon = false}: FollowButtonProps<T>) {
-  const _onPress = useCallback(() => onPress(id), [id, onPress]);
+export default function FollowButton<T>({id, onPress, isFollowing, loading = false, hideIcon = false}: FollowButtonProps<T>) {
+  const _onPress = useCallback(() => onPress({id, isFollowing}), [id, isFollowing, onPress]);
   
   const btnProps: ButtonProps = {
-    children: 'Follow',
-    mode: "contained",
+    children: isFollowing ? "Unfollow" : "Follow",
+    mode: isFollowing ? "outlined" : "contained",
+    loading,
     onPress: _onPress,
   };
   
