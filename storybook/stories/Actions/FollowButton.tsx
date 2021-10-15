dc28277a7ba0bd0a18e632bdbf94cfdd2fc219fb
Kiruse <contact@kiruse.dev>
2021-10-15 13:57:43 +0200

    improve default styling & prop forwarding

--- storybook/stories/Actions/FollowButton.tsx
+++ storybook/stories/Actions/FollowButton.tsx
@@ -8,23 +8,33 @@ import { StyleProp, TextStyle } from 'react-native'
 import { Icon } from 'react-native-elements'
 import { Button, ButtonProps } from '~comps/Typography'
 
-export type FollowButtonProps<T> = {
+export type FollowButtonProps<T> = Omit<ButtonProps, 'children' | 'onPress'> & {
   /** Context-sensitive ID to pass back upon press. The context should know what the ID belongs to (Account, Space). */
   id: T
   onPress: (props: {id: T, isFollowing: boolean}) => void
   isFollowing: boolean
   loading?: boolean
   hideIcon?: boolean
-  labelStyle?: StyleProp<TextStyle>
 }
-export default function FollowButton<T>({id, onPress, isFollowing, loading = false, hideIcon = false}: FollowButtonProps<T>) {
+export default function FollowButton<T>({
+  id,
+  onPress,
+  isFollowing,
+  loading = false,
+  hideIcon = false,
+  labelStyle,
+  ...props
+}: FollowButtonProps<T>)
+{
   const _onPress = useCallback(() => onPress({id, isFollowing}), [id, isFollowing, onPress]);
   
   const btnProps: ButtonProps = {
+    ...props,
     children: isFollowing ? "Unfollow" : "Follow",
     mode: isFollowing ? "outlined" : "contained",
     loading,
     onPress: _onPress,
+    labelStyle: [{fontSize: 15}, !hideIcon && {marginLeft: 8}, labelStyle]
   };
   
   // need this branch because the mere presence of the "icon" prop already alters appearance
