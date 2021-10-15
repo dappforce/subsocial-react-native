926f8192af33320456b4dfb315f3911ebce26708
Kiruse <contact@kiruse.dev>
2021-10-15 16:31:36 +0200

    add Post Age helper

--- src/util.ts
+++ src/util.ts
@@ -1,5 +1,6 @@
 //////////////////////////////////////////////////////////////////////
 // Simple utility functions independent from RN
+import { BN, BN_ZERO } from '@polkadot/util'
 
 export type IconFamily = 'antdesign' | 'entypo' | 'evilicon' |
   'feather' | 'font-awesome' | 'font-awesome-5' | 'fontisto' |
@@ -22,3 +23,40 @@ export function partition<T>(ary: T[], predicate: (elem: T) => boolean) {
     }
   }, [[], []]);
 }
+
+export class Age {
+  private timestamp: BN
+  private now: BN
+  
+  constructor(timestamp: BN) {
+    this.timestamp = timestamp;
+    this.now = new BN(Date.now());
+  }
+  
+  toString(): string {
+    const diff = this.now.sub(this.timestamp);
+    if (diff.lt(BN_ZERO)) {
+      return 'in the future'
+    }
+    if (diff.lt(BN_1DAY)) {
+      return diff.divn(3600) + 'h'
+    }
+    if (diff.lt(BN_1WEEK)) {
+      return diff.div(BN_1DAY) + 'd'
+    }
+    if (diff.lt(BN_1MONTH)) {
+      return diff.div(BN_1WEEK) + 'w'
+    }
+    if (diff.lt(BN_1YEAR)) {
+      return diff.div(BN_1MONTH) + 'M'
+    }
+    else {
+      return diff.div(BN_1YEAR) + 'y'
+    }
+  }
+}
+
+const BN_1DAY   = new BN(86400000)
+const BN_1WEEK  = BN_1DAY.muln(7)
+const BN_1MONTH = BN_1DAY.muln(30)
+const BN_1YEAR  = BN_1DAY.muln(365)
