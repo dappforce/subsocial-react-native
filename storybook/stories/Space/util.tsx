62f8461542821213563eb8ba56464154cbedff03
Kiruse <contact@kiruse.dev>
2021-10-19 15:14:42 +0200

    using IPFS cache in useSpace useSpace, however, is not the only way we access spaces

--- storybook/stories/Space/util.tsx
+++ storybook/stories/Space/util.tsx
@@ -1,10 +1,13 @@
 //////////////////////////////////////////////////////////////////////
 // useSpace helper - a specialization of useSubsocialInitializer for
 // loading space data
-import { SubsocialSubstrateApi } from '@subsocial/api'
+import { SubsocialApi, SubsocialSubstrateApi } from '@subsocial/api'
 import { SubsocialInitializerState, useSubsocialInitializer } from '~comps/SubsocialContext'
 import { AnySpaceId, SpaceData } from '@subsocial/types'
+import { Visibility } from '@subsocial/api/filters'
+import { partition } from '~src/util'
 import { BN } from '@polkadot/util'
+import { cacheSpaces, querySpaceCache } from '~src/IpfsCache'
 
 /**
  * Unified space ID, from numbers, BNs, number strings & handles.
@@ -20,11 +23,37 @@ export class SpaceNotFoundError extends Error {
 export function useSpace(id: UnifiedSpaceId): [SubsocialInitializerState, SpaceData|undefined] {
   return useSubsocialInitializer(async api => {
     if (!id) return undefined;
-    const bnid  = await resolveSpaceId(api.substrate, id);
-    const data = await api.findSpace({id: bnid});
-    if (!data) throw new SpaceNotFoundError(bnid.toNumber());
+    const [data] = await loadSpaces(api, [id]);
+    if (!data) throw new SpaceNotFoundError(id);
     return data;
   }, [id]);
+}
+
+export type LoadSpacesOptions = {
+  visibility?: Visibility
+}
+export async function loadSpaces(api: SubsocialApi, ids: UnifiedSpaceId[], {visibility = 'onlyPublic'}: LoadSpacesOptions = {}): Promise<SpaceData[]> {
+  const resolved = await resolveSpaceIds(api.substrate, ids); // returns immediately if already resolved
+  const structs  = await api.substrate.findSpaces({ids: resolved, visibility});
+  const [withCid, withoutCid] = partition(structs, struct => struct.content.isIpfs);
+  if (withoutCid.length) {
+    console.warn('some spaces have non-IPFS CIDs:', withoutCid);
+  }
+  
+  const cached = await querySpaceCache(withCid.map(struct => struct.content.asIpfs.toString()));
+  
+  // fetch uncached contents & cache them - if possible
+  const missing = withCid.filter(struct => !(struct.content.asIpfs.toString() in cached));
+  const fetched = await api.ipfs.findSpaces(missing.map(struct => struct.content.asIpfs.toString()));
+  cacheSpaces(fetched);
+  
+  return withCid.map(struct => {
+    const cid = struct.content.asIpfs.toString();
+    return {
+      struct,
+      content: cached[cid] ?? fetched[cid],
+    };
+  });
 }
 
 export async function resolveSpaceId(substrate: SubsocialSubstrateApi, id: UnifiedSpaceId): Promise<BN> {
@@ -37,6 +66,9 @@ export async function resolveSpaceId(substrate: SubsocialSubstrateApi, id: Unifi
   else {
     return new BN(id);
   }
+}
+export function resolveSpaceIds(substrate: SubsocialSubstrateApi, ids: UnifiedSpaceId[]): Promise<BN[]> {
+  return Promise.all(ids.map(id => resolveSpaceId(substrate, id)));
 }
 
 const isHandle = (id: UnifiedSpaceId) => typeof id === 'string' && id.startsWith('@')
