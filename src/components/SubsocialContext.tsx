d1b3d8a36b7e7117b6a8ee835ddc4dd1842f1158
Kiruse <kiruse@kiruse.dev>
2021-11-22 19:20:36 +0100

    improve SubsocialProvider - display loading indicator w/ message until API is ready - display error message if applicable - error message takes precedence over apparent success

--- src/components/SubsocialContext.tsx
+++ src/components/SubsocialContext.tsx
@@ -2,9 +2,13 @@
 // Simple Substrate API integration with RN
 // SPDX-License-Identifier: GPL-3.0
 import React, { useCallback, useContext, useEffect, useReducer, useState } from 'react'
+import { StyleSheet, View } from 'react-native'
+import { ActivityIndicator } from 'react-native-paper'
 import { SubsocialApi } from '@subsocial/api'
 import { SubstrateState, useSubstrate } from './SubstrateContext'
 import config from 'config.json'
+import { useTheme } from './Theming'
+import { Text } from './Typography'
 
 export { SubstrateProvider, useSubstrate } from './SubstrateContext'
 
@@ -14,7 +18,7 @@ type SubsocialConnectionState = 'PENDING' | 'CONNECTED' | 'ERROR'
 
 export type SubsocialState = React.PropsWithChildren<{
   substrate: SubstrateState
-  api?: SubsocialApi
+  api: SubsocialApi
   apiError?: any
   connectionState: SubsocialConnectionState
 }>
@@ -26,34 +30,64 @@ type StateAction = {
 }
 
 export const SubsocialContext = React.createContext<SubsocialState>(undefined as unknown as SubsocialState)
-export const useSubsocial = () => useContext(SubsocialContext);
+export const useSubsocial = () => useContext(SubsocialContext)
 
 
 // ===== Provider =====
 
 export type SubsocialProviderProps = React.PropsWithChildren<{}>
 
-export function SubsocialProvider({children}: SubsocialProviderProps) {
+export function SubsocialProvider({ children }: SubsocialProviderProps) {
   const substrate = useSubstrate()
-  const [ state, dispatch ] = useReducer(stateReducer, { substrate, api: undefined, connectionState: 'PENDING' })
-  const { api } = state
+  const theme = useTheme()
+  const [ state, dispatch ] = useReducer(stateReducer, { substrate, api: null as unknown as SubsocialApi, connectionState: 'PENDING' })
+  const { api, apiError } = state
   
-  switch (substrate?.connectionState) {
-    case 'READY': {
-      if (!api) dispatch({ type: 'CONNECT', data: { substrate } })
-      break
+  useEffect(() => {
+    switch (substrate?.connectionState) {
+      case 'READY': {
+        if (!api) dispatch({ type: 'CONNECT', data: { substrate } })
+        break
+      }
+      case 'ERROR': {
+        dispatch({ type: 'ERROR', data: substrate.apiError })
+        console.error(substrate.apiError)
+        break
+      }
     }
-    case 'ERROR': {
-      dispatch({ type: 'ERROR', data: substrate.apiError })
-      break
-    }
+  }, [ substrate?.connectionState ])
+  
+  if (!api && !apiError) {
+    return (
+      <View style={styles.coverContainer}>
+        <Text style={styles.centerText}>
+          Connecting to the network ...
+        </Text>
+        <ActivityIndicator color={theme.colors.primary} />
+      </View>
+    )
   }
   
-  return (
-    <SubsocialContext.Provider value={state}>
-      {children}
-    </SubsocialContext.Provider>
-  )
+  else if (apiError) {
+    return (
+      <View style={styles.coverContainer}>
+        <Text style={[styles.centerText, { fontFamily: 'RobotoMedium' }]}>
+          An error occurred while attempting to connect to the network.
+        </Text>
+        <Text style={styles.centerText}>
+          The gateway node may be temporarily unavailable. Otherwise, please ensure you enjoy a stable Internet connection.
+        </Text>
+      </View>
+    )
+  }
+  
+  else {
+    return (
+      <SubsocialContext.Provider value={state}>
+        {children}
+      </SubsocialContext.Provider>
+    )
+  }
 }
 
 function stateReducer(state: SubsocialState, action: StateAction): SubsocialState {
@@ -75,7 +109,7 @@ function stateReducer(state: SubsocialState, action: StateAction): SubsocialStat
       }
     }
     case 'ERROR': {
-      return { ...state, api: undefined, apiError: action.data, connectionState: 'ERROR' }
+      return { ...state, api: null as unknown as SubsocialApi, apiError: action.data, connectionState: 'ERROR' }
     }
     default: throw new Error(`unknown Subsocial context action ${action.type}`)
   }
@@ -146,3 +180,18 @@ export function isLoading(state: SubsocialInitializerState) {
 
 export const isReady = (state: SubsocialInitializerState) => state === 'READY'
 export const isError = (state: SubsocialInitializerState) => state === 'ERROR'
+
+const styles = StyleSheet.create({
+  coverContainer: {
+    flex: 1,
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  centerText: {
+    textAlign: 'center',
+    maxWidth: '80%',
+    marginBottom: 20,
+  },
+})
