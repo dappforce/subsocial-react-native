cbfbb38f9704315c416e9a32ac50c1cab11f0874
Kiruse <kiruse@kiruse.dev>
2021-12-08 17:58:51 +0100

    minifix: PostStructWithRoot's rootPostId is a PostId, not a number

--- src/types/subsocial.ts
+++ src/types/subsocial.ts
@@ -4,8 +4,8 @@ export * from '@subsocial/api/flat-subsocial'
 export * from '@subsocial/api/flat-subsocial/dto'
 export * from '@subsocial/api/flat-subsocial/flatteners'
 export * from '@subsocial/api/flat-subsocial/utils'
-import { PostStruct } from './subsocial'
+import { PostId, PostStruct } from './subsocial'
 
 export type PostStructWithRoot = PostStruct & {
-    rootPostId: number
+    rootPostId: PostId
 }
